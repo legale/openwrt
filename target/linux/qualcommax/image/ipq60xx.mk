0cc11973e13da82919574d50153c9f353a2e1924
Isaev Ruslan <legale.legale@gmail.com>
2023-11-12 00:26:12 +0300

    ipq60xx: add support yuncore ax840
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -9,3 +9,16 @@ define Device/linksys_mr7350
 	DEVICE_PACKAGES := kmod-leds-pca963x
 endef
 TARGET_DEVICES += linksys_mr7350
+
+define Device/yuncore_ax840
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Yuncore
+	DEVICE_MODEL := AX840
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq6018
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	DEVICE_PACKAGES := ath11k-firmware-ipq6018 ipq-wifi-yuncore_ax840 uboot-env
+endef
+TARGET_DEVICES += yuncore_ax840
--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -4,13 +4,24 @@ REQUIRE_IMAGE_METADATA=1
 RAMFS_COPY_BIN='fw_printenv fw_setenv head'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
+
 platform_check_image() {
-	return 0;
+	local magic_long="$(get_magic_long "$1")"
+	board=$(board_name)
+	case $board in
+	yuncore,ax840)
+			[ "$magic_long" = "73797375" ] && return 0
+			;;
+	esac
+	return 1
 }
 
 platform_do_upgrade() {
 	case "$(board_name)" in
-	linksys,mr7350)
+	linksys,mr7350|\
+	yuncore,ax840)
+		CI_UBIPART="rootfs"
+		CI_ROOTPART="ubi_rootfs"
 		nand_do_upgrade "$1"
 		;;
 	*)
