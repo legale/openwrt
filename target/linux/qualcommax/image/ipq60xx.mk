55d5f378cfd7e99a1a45d8e584ed24b5ee5f7070
Isaev Ruslan <legale.legale@gmail.com>
2023-11-12 00:26:12 +0300

    ipq60xx: add support yuncore ax840
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -45,7 +45,6 @@ define Device/glinet_gl-axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
 
-
 define Device/jdc_ax1800-pro
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)
@@ -61,3 +60,16 @@ define Device/jdc_ax1800-pro
 	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$(BLOCKSIZE)
 endef
 TARGET_DEVICES += jdc_ax1800-pro
+
+define Device/yuncore_ax840
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Yuncore
+	DEVICE_MODEL := AX840
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	SOC := ipq6018
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	DEVICE_PACKAGES := ath11k-firmware-ipq6018 ipq-wifi-yuncore_ax840 uboot-env
+endef
+TARGET_DEVICES += yuncore_ax840
--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -12,12 +12,23 @@ platform_check_image() {
 platform_pre_upgrade() {
 	case "$(board_name)" in
 	esac
+	local magic_long="$(get_magic_long "$1")"
+	board=$(board_name)
+	case $board in
+	yuncore,ax840)
+			[ "$magic_long" = "73797375" ] && return 0
+			;;
+	esac
+	return 1
 }
 
 platform_do_upgrade() {
 	case "$(board_name)" in
 	glinet,gl-axt1800|\
-	glinet,gl-ax1800)
+	glinet,gl-ax1800|\
+	yuncore,ax840)
+		CI_UBIPART="rootfs"
+		CI_ROOTPART="ubi_rootfs"
 		nand_do_upgrade "$1"
 		;;
 	jdc,ax1800-pro)
