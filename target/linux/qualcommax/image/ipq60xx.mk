8c77858d6dd61f5b8f1561b6087e56a6991c8894
Isaev Ruslan <legale.legale@gmail.com>
2024-01-03 23:52:54 +0300

    ipq6018: add yuncore fap650 support
    This adds initial support for the YunCore FAP650 AP.
    
    Specifications:
    
    - Qualcomm IPQ6018++QCA8075+QCN5022+QCN5052
    - 512 MB of RAM (DDR3)
    - 8 MB of serial flash (SPI NOR)
    - 128 MB of parallel flash (NAND)
    - 2x2 2.4 GHz WiFi (IPQ6010)
    - 2x2 5 GHz WiFi (IPQ6010)
    - 2x 2dBi 2.4G MIMO antenna
    - 2x 3dBi 5.8G MIMO antenna
    - 5x 1 Gbps Ethernet (QCA8075)
    - POE: 48V (IEEE 802.3af)
    - power: 12V (~1.5A)
    - 1x passthru port (rj45 - rj45)
    - 1x cisco rj45 console port
    - size: 160mm*86mm*29mm
    
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -75,6 +75,19 @@ define Device/jdc_ax1800-pro
 endef
 TARGET_DEVICES += jdc_ax1800-pro
 
+define Device/yuncore_fap650
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Yuncore
+	DEVICE_MODEL := FAP650
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-yuncore_fap650
+endef
+TARGET_DEVICES += yuncore_fap650
+
 define Device/yuncore_ax840
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
