0ef5b9b9cfaf9d0d1bbd1287226727ef45ea5045
Isaev Ruslan <legale.legale@gmail.com>
2024-02-28 10:53:54 +0300

    ipq60xx: add yuncore fap650 support
    This commit adds support for the Yuncore FAP650 device.
    
    Specifications:
    - Qualcomm IPQ6018+QCA8075+QCN5022+QCN5052
    - 512 MB of RAM (DDR3)
    - 8 MB of serial flash (SPI NOR)
    - 128 MB of parallel flash (NAND)
    - 2x2 2.4 GHz WiFi (IPQ6010)
    - 2x2 5 GHz WiFi (IPQ6010)
    - 2x 2dBi 2.4G MIMO antenna
    - 2x 3dBi 5.8G MIMO antenna
    - 5x 1 Gbps Ethernet (QCA8075)
    - POE: 48V (IEEE 802.3af)
    - power: 12V (~1.5A)
    - 1x passthru port (rj45 - rj45)
    - 1x cisco rj45 console port
    - size: 160mm*86mm*29mm
    
    BACKUP YOUR STOCK FIRMWARE:
    ```
    export device=fa650
    mkdir -p /tmp/fw_dump_$device
    cd /tmp/fw_dump_$device
    dmesg > dmesg_$device.log
    dtc -I fs /sys/firmware/devicetree/base > $device.dts
    cat /proc/device-tree/model > model
    cat /proc/mtd > proc_mtd
    while read p; do
    mtd_dev=$(echo $p | cut -d: -f1)
    echo $mtd_dev
    dd if=/dev/$mtd_dev of=$mtd_dev
    done < proc_mtd
    md5sum * > md5sum.log
    tar -cvzf ../$device.tar.gz .
    export sum=$(md5sum /tmp/$device.tar.gz | cut -d' ' -f1)
    mv ../$device.tar.gz /tmp/${device}_${sum}.tar.gz
    echo fw backup saved to: /tmp/${device}_${sum}.tar.gz
    ```
    Upload your backup via tftp to the safe place.
    
    INSTALLATION:
    1. stock firmware web ui
    Rename factory.bin fw image file to factory.ubin. Flash this image
    like ordinary stock fw upgrade.
    
    2. stock firmware telnet method
    Enter telnet cli (login: root, password: 476t*_f0%g09y) and upload
     factory.bin fw image and rename it to factory.ubin
    `cd /tmp && wget <your_web_server_ip>/factory.ubin`
    `sysupgrade factory.ubin
    
    3. initramfs method
        Put imitramfs image to your TFTP server and rename it for example to fap650.initram
        Enable serial console and enter to the u-boot cli.
        Exec these commands:
        `tftpboot <your_tftp_server_ip>:fap650.initram`
        `dhcp`
    
        When downloading is finished:
        `bootm`
        After booting the device, you need to upload to the device factory.ubi fw image.
        ```
        cd /tmp && wget <your_web_server_ip>/factory.ubi`
        export rootfs=$(cat /proc/mtd | grep rootfs | cut -d: -f1)
        export rootfs_1=$(cat /proc/mtd | grep rootfs_1 | cut -d: -f1)
        ubiformat /dev/${rootfs} -y -f factory.ubi
        ubiformat /dev/${rootfs_1} -y -f factory.ubi
        reboot
        ```
    
    4. u-boot factory.ubi image method
        Put factory.ubi to your TFTP server
        Enter u-boot cli and exec these commands:
        `tftpboot <your_tftp_server_ip>:factory.ubi`
        `dhcp`
        After downloading is finished:
        `flash rootfs`
        `flash rootfs_1`
        `reset`
    
    STOCK FIRMWARE RECOVERY:
    Boot initramfs image.
    Upload your rootfs mtd partition to the device using scp or download
    it from the device using wget.
    Enter device ssh cli and exec:
    ```
    cd /tmp && wget <your_web_server_ip>/rootfs_mtd`
    export rootfs=$(cat /proc/mtd | grep rootfs | cut -d: -f1)
    export rootfs_1=$(cat /proc/mtd | grep rootfs_1 | cut -d: -f1)
    ubiformat /dev/${rootfs} -y -f /tmp/rootfs_mtd
    ubiformat /dev/${rootfs_1} -y -f /tmp/rootfs_mtd
    reboot
    ```
    
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -10,3 +10,19 @@ define Device/8devices_mango-dvk
 	DEVICE_PACKAGES := ipq-wifi-8devices_mango
 endef
 TARGET_DEVICES += 8devices_mango-dvk
+
+define Device/yuncore_fap650
+    $(call Device/FitImage)
+    $(call Device/UbiFit)
+    DEVICE_VENDOR := Yuncore
+    DEVICE_MODEL := FAP650
+    BLOCKSIZE := 128k
+    PAGESIZE := 2048
+    DEVICE_DTS_CONFIG := config@cp03-c1
+    SOC := ipq6018
+    DEVICE_PACKAGES := ipq-wifi-yuncore_fap650
+    IMAGES += factory.ubin
+    IMAGE/factory.ubin := append-ubi | qsdk-ipq-factory-nand
+endef
+TARGET_DEVICES += yuncore_fap650
+
