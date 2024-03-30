b45171836de5f81ed06ca66aba296ab00af8cc81
Isaev Ruslan <legale.legale@gmail.com>
2024-03-31 00:22:49 +0300

    qualcommax: ipq60xx: add yuncore ax840-p2 support
    This commit adds support for the Yuncore AX840-p2 device.
    
    Specifications:
    - Qualcomm IPQ6018+QCA8075+QCN5022+QCN5052
    - 512 MB of RAM (DDR3)
    - 8 MB of serial flash (SPI NOR)
    - 128 MB of parallel flash (NAND)
    - 2x2 2.4 GHz WiFi (IPQ6010)
    - 2x2 5 GHz WiFi (IPQ6010)
    - 2x IPEX/U.FL antenna connectors
    - 4x 3 dBi dual-band antennas
    - 2x 1 Gbps Ethernet (QCA8075) with 802.3at PoE input support
    - POE: 48V (IEEE 802.3af)
    - power: 12V (~1.5A)
    - 1x RGB LED
    - 1x push-button (reset)
    - DC jack for main power input (12 V)
    - UART (4-pin, 2.54 mm pitch) header on PCB
    - power Consumption: ~24W
    - size: 198mm*198mm*41.02mm
    
    BACKUP YOUR STOCK FIRMWARE:
    ```
    export device=ax840
    mkdir -p /tmp/fw_dump_$device
    cd /tmp/fw_dump_$device
    dmesg > dmesg_$device.log
    dtc -I fs /sys/firmware/devicetree/base > $device.dts
    cat /proc/device-tree/model > model
    cat /proc/mtd > proc_mtd
    while read p; do
    mtd_dev=$(echo $p | cut -d: -f1)
    echo $mtd_dev
    dd if=/dev/$mtd_dev of=$mtd_dev
    done < proc_mtd
    md5sum * > md5sum.log
    tar -cvzf ../$device.tar.gz .
    export sum=$(md5sum /tmp/$device.tar.gz | cut -d' ' -f1)
    mv ../$device.tar.gz /tmp/${device}_${sum}.tar.gz
    echo fw backup saved to: /tmp/${device}_${sum}.tar.gz
    ```
    Upload your backup via tftp to the safe place.
    
    INSTALLATION:
    1. stock firmware web ui
    Rename factory.bin fw image file to factory.ubin. Flash this image
    like ordinary stock fw upgrade.
    
    2. stock firmware telnet method
    Enter telnet cli (login: root, password: 476t*_f0%g09y) and upload
     factory.bin fw image and rename it to factory.ubin
    `cd /tmp && wget <your_web_server_ip>/factory.ubin`
    `sysupgrade factory.ubin
    
    3. initramfs method
        Put imitramfs image to your TFTP server and rename it for example to fap650.initram
        Enable serial console and enter to the u-boot cli.
        Exec these commands:
        `tftpboot <your_tftp_server_ip>:fap650.initram`
        `dhcp`
    
        When downloading is finished:
        `bootm`
        After booting the device, you need to upload to the device factory.ubi fw image.
        ```
        cd /tmp && wget <your_web_server_ip>/factory.ubi`
        export rootfs=$(cat /proc/mtd | grep rootfs | cut -d: -f1)
        export rootfs_1=$(cat /proc/mtd | grep rootfs_1 | cut -d: -f1)
        ubiformat /dev/${rootfs} -y -f factory.ubi
        ubiformat /dev/${rootfs_1} -y -f factory.ubi
        reboot
        ```
    
    4. u-boot factory.ubi image method
        Put factory.ubi to your TFTP server
        Enter u-boot cli and exec these commands:
        `tftpboot <your_tftp_server_ip>:factory.ubi`
        `dhcp`
        After downloading is finished:
        `flash rootfs`
        `flash rootfs_1`
        `reset`
    
    STOCK FIRMWARE RECOVERY:
    Boot initramfs image.
    Upload your rootfs mtd partition to the device using scp or download
    it from the device using wget.
    Enter device ssh cli and exec:
    ```
    cd /tmp && wget <your_web_server_ip>/rootfs_mtd`
    export rootfs=$(cat /proc/mtd | grep rootfs | cut -d: -f1)
    export rootfs_1=$(cat /proc/mtd | grep rootfs_1 | cut -d: -f1)
    ubiformat /dev/${rootfs} -y -f /tmp/rootfs_mtd
    ubiformat /dev/${rootfs_1} -y -f /tmp/rootfs_mtd
    reboot
    ```
    
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -13,6 +13,19 @@ platform_do_upgrade() {
 	netgear,wax214)
 		nand_do_upgrade "$1"
 		;;
+	yuncore,ax840-p2|\
+	yuncore,fap650)
+		[ "$(fw_printenv -n owrt_env_ver 2>/dev/null)" != "7" ] && yuncore_fap650_env_setup
+		local active="$(fw_printenv -n owrt_slotactive 2>/dev/null)"
+		if [ "$active" = "1" ]; then
+			CI_UBIPART="rootfs"
+		else
+			CI_UBIPART="rootfs_1"
+		fi
+		fw_setenv owrt_bootcount 0
+		fw_setenv owrt_slotactive $((1 - active))
+		nand_do_upgrade "$1"
+		;;
 	*)
 		default_do_upgrade "$1"
 		;;
