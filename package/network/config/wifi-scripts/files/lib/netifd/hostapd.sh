65a8606b7dd8f6d9c89d5315398d501346d56040
Isaev Ruslan <legale.legale@gmail.com>
2024-12-08 20:18:30 +0300

    hostapd.sh: add radius support
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- package/network/config/wifi-scripts/files/lib/netifd/hostapd.sh
+++ package/network/config/wifi-scripts/files/lib/netifd/hostapd.sh
@@ -566,8 +566,8 @@ hostapd_set_bss_options() {
 		wps_independent wps_device_type wps_device_name wps_manufacturer wps_pin \
 		macfilter ssid utf8_ssid uapsd hidden short_preamble rsn_preauth \
 		iapp_interface eapol_version dynamic_vlan ieee80211w nasid \
-		acct_secret acct_port acct_interval \
-		bss_load_update_period chan_util_avg_period sae_require_mfp sae_pwe sae_track_password \
+		acct_secret acct_port acct_interval auth_secret auth_port \
+		bss_load_update_period chan_util_avg_period sae_require_mfp sae_pwe \
 		multi_ap multi_ap_backhaul_ssid multi_ap_backhaul_key skip_inactivity_poll \
 		ppsk airtime_bss_weight airtime_bss_limit airtime_sta_weight \
 		multicast_to_unicast_all proxy_arp per_sta_vif na_mcast_to_ucast \
@@ -641,8 +641,12 @@ hostapd_set_bss_options() {
 
 	[ -n "$acct_interval" ] && \
 		append bss_conf "radius_acct_interim_interval=$acct_interval" "$N"
+	set_default acct_port 1812
+	set_default auth_port 1812
 	json_for_each_item append_acct_server acct_server
 	json_for_each_item append_radius_acct_req_attr radius_acct_req_attr
+	json_for_each_item append_auth_server auth_server
+	json_for_each_item append_radius_auth_req_attr radius_auth_req_attr
 
 	[ -n "$ocv" ] && append bss_conf "ocv=$ocv" "$N"
 	[ -n "$beacon_prot" ] && append bss_conf "beacon_prot=$beacon_prot" "$N"
@@ -724,7 +728,6 @@ hostapd_set_bss_options() {
 		;;
 		eap|eap2|eap-eap2|eap192)
 			json_get_vars \
-				auth_server auth_secret auth_port \
 				dae_client dae_secret dae_port \
 				dynamic_ownip ownip radius_client_addr \
 				eap_reauth_period request_cui \
@@ -735,11 +738,6 @@ hostapd_set_bss_options() {
 			vlan_possible=1
 
 			set_default dynamic_ownip 1
-
-			# legacy compatibility
-			[ -n "$auth_server" ] || json_get_var auth_server server
-			[ -n "$auth_port" ] || json_get_var auth_port port
-			[ -n "$auth_secret" ] || json_get_var auth_secret key
 
 			[ "$fils" -gt 0 ] && {
 				set_default erp_domain "$mobility_domain"
@@ -765,7 +763,6 @@ hostapd_set_bss_options() {
 				[ -n "$fils_dhcp" ] && append bss_conf "dhcp_server=$fils_dhcp" "$N"
 			}
 
-			set_default auth_port 1812
 			set_default dae_port 3799
 			set_default request_cui 0
 
@@ -777,7 +774,6 @@ hostapd_set_bss_options() {
 				append bss_conf "radius_das_port=$dae_port" "$N"
 				append bss_conf "radius_das_client=$dae_client $dae_secret" "$N"
 			}
-			json_for_each_item append_radius_auth_req_attr radius_auth_req_attr
 
 			if [ -n "$ownip" ]; then
 				append bss_conf "own_ip_addr=$ownip" "$N"
